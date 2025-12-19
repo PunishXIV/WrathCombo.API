a5491664c3ba980d3843a8a6c5bb06461d80e1e1
Ethan Henderson <ethan@zbee.codes>
2025-12-19 15:05:04 -0700

    style(Enums): Rename `BailMessages`

--- Enum/BailMessage.cs
+++ Enum/BailMessage.cs
@@ -6,14 +6,16 @@ using System.ComponentModel;
 
 namespace WrathCombo.API.Enum;
 
-public enum BailMessages
+public enum BailMessage
 {
     [Description("IPC services are currently disabled.")]
     LiveDisabled = 0,
 
-    [Description("Invalid lease.")] InvalidLease = 1,
+    [Description("Invalid lease.")]
+    InvalidLease = 1,
 
-    [Description("Blacklisted lease.")] BlacklistedLease = 2,
+    [Description("Blacklisted lease.")]
+    BlacklistedLease = 2,
 
     [Description("Not enough configurations available.")]
     NotEnoughConfigurations = 3,
