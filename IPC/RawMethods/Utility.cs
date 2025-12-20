73c067d71394ef82f767de1c014f6d5125f44aa6
Ethan Henderson <ethan@zbee.codes>
2025-12-20 12:53:26 -0700

    feat(RawIPC): Added `Wrath.IPC.Utility` methods
    Signed-off-by: Ethan Henderson <ethan@zbee.codes>

---
+++ IPC/RawMethods/Utility.cs
@@ -0,0 +1,28 @@
+﻿#region
+
+using Dalamud.Plugin.Ipc;
+
+// ReSharper disable RedundantSuppressNullableWarningExpression
+// ReSharper disable StaticMemberInitializerReferesToMemberBelow
+
+#endregion
+
+namespace WrathCombo.API.IPC.RawMethods;
+
+public partial class RawMethod
+{
+    internal static readonly ICallGateSubscriber<float?, bool>
+        CanWeave =
+            Interface!.GetIpcSubscriber<float?, bool>
+                ($"{WC}CanWeave");
+
+    internal static readonly ICallGateSubscriber<float?, float?, bool>
+        CanDelayedWeave =
+            Interface!.GetIpcSubscriber<float?, float?, bool>
+                ($"{WC}CanDelayedWeave");
+
+    internal static readonly ICallGateSubscriber<uint, bool?, bool?, bool>
+        ActionReady =
+            Interface!.GetIpcSubscriber<uint, bool?, bool?, bool>
+                ($"{WC}ActionReady");
+}
