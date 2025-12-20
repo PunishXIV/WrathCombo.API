d765d87640b014eede62c0ddf93777541d65f39d
Ethan Henderson <ethan@zbee.codes>
2025-12-20 12:54:03 -0700

    feat(Wrapper): Added Wrappers for `Wrath.IPC.Utility` methods
    Signed-off-by: Ethan Henderson <ethan@zbee.codes>

---
+++ IPC/Utility.cs
@@ -0,0 +1,22 @@
+﻿#region
+
+using WrathCombo.API.IPC.RawMethods;
+
+#endregion
+
+namespace WrathCombo.API;
+
+public static partial class WrathIPCWrapper
+{
+    public static bool CanWeave(float? estimatedWeaveTime = null) =>
+        SafeInvokeRawMethod(() => RawMethod.CanWeave.InvokeFunc(estimatedWeaveTime));
+
+    public static bool CanDelayedWeave(float? weaveStart, float? weaveEnd) =>
+        SafeInvokeRawMethod(() =>
+            RawMethod.CanDelayedWeave.InvokeFunc(weaveStart, weaveEnd));
+
+    public static bool
+        ActionReady(uint actionId, bool? recastCheck, bool? castCheck) =>
+        SafeInvokeRawMethod(() =>
+            RawMethod.ActionReady.InvokeFunc(actionId, recastCheck, castCheck));
+}
