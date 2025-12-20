7d17d2d95d8611919857d94c93dfefb0391b155e
Ethan Henderson <ethan@zbee.codes>
2025-12-20 13:31:24 -0700

    feat(RawIPC): Added `Wrath.IPC.AutoRot` methods
    Signed-off-by: Ethan Henderson <ethan@zbee.codes>

---
+++ IPC/RawMethods/AutoRotationConfiguration.cs
@@ -0,0 +1,26 @@
+﻿#region
+
+using Dalamud.Plugin.Ipc;
+using WrathCombo.API.Enum;
+
+// ReSharper disable RedundantSuppressNullableWarningExpression
+// ReSharper disable StaticMemberInitializerReferesToMemberBelow
+
+#endregion
+
+namespace WrathCombo.API.IPC.RawMethods;
+
+public partial class RawMethod
+{
+    internal static readonly
+        ICallGateSubscriber<object, object>
+        GetAutoRotationConfigState =
+            Interface!.GetIpcSubscriber<object, object>
+                ($"{WC}GetAutoRotationConfigState");
+
+    internal static readonly
+        ICallGateSubscriber<Guid, object, object, SetResult>
+        SetAutoRotationConfigState =
+            Interface!.GetIpcSubscriber<Guid, object, object, SetResult>
+                ($"{WC}SetAutoRotationConfigState");
+}
